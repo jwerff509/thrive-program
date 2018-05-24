b8eae5f176f3f486b73a2f197c123f537edf4047
Justin Vanderwerff <vanderwerffj@gmail.com>
2018-05-24 10:35:10 -0400

    Updates to PPI Data entry.

--- resources/views/group_surveys/partials/_form2.blade.php
+++ resources/views/group_surveys/partials/_form2.blade.php
@@ -509,13 +509,8 @@
 
 
 <div class="form-group row">
-<<<<<<< HEAD
   {!! Form::label('question_1', 'PPI1: Total Household Members', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-  {!! Form::label('question_1', 'How many members does the household have:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_1', array(
       '0' => 'Eight or more',
       '7' => 'Seven',
@@ -525,39 +520,21 @@
       '21' => 'Three',
       '29' => 'One or two',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_2', 'PPI2: Are All members aged 7 to 16 currently attending school', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_2', 'Are all household members aged 7 to 16 currently attending school:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_2', array(
       '0' => 'No',
       '3' => 'Yes',
       '6' => 'No one 7 to 16',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_3', 'PPI3: Highest grade that the female head/spouse has attained', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_3', 'What is the highest grade that the female head/spouse has attained:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_3', array(
       '0' => 'None, or first to fifth grade',
       '2' => 'Sixth grade',
@@ -565,142 +542,75 @@
       '9' => 'Tenth grade or higher',
       '5' => 'No female head/spouse',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_4', 'PPI4: Floor material', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_4', 'What kind of building material is the floor of this dwelling made of:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_4', array(
       '0' => 'Mud, wood only, or other',
       '2' => 'Concrete, or covered concrete',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_5', 'PPI5: Roof Material', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_5', 'What kind of building material is the roof of this dwelling made of:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_5', array(
       '0' => 'Grass/straw/thatch, or other',
       '3' => 'Iron sheets, or other non-asbestos tiles',
       '5' => 'Concrete, asbestos sheets, or asbestos tiles',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_6', 'PPI6: Main Energy Type Used for Cooking', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_6', 'What is the main type of energy that your household uses for cooking:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_6', array(
       '0' => 'Firewood, coal, crop/livestock residues, or other',
       '4' => 'Charcoal',
       '15' => 'Gas, electricity, solar, or kerosene/paraffin',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_7', 'PPI7: TVs, DVDs/VCRs or home theatres, or satellite dish/decoders', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_7', 'Does your household own any televisions, DVDs/VCRs or home theatres, or satellite dish/decoders (free to air, or DSTV) or other pay-TV arrangements:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_7', array(
       '0' => 'No TVs (regardless of others)',
       '6' => 'TV, but nothing else',
       '10' => 'TV, and something else (DVD, dish, etc.)',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_8', 'PPI8: Non-electric or electric irons', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_8', 'Does your household own any non-electric or electric irons:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_8', array(
       '0' => 'None',
       '4' => 'Only non-electric',
       '11' => 'Electric, or both electric and non-electric',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_9', 'PPI9: Cellular phones', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_9', 'Does your household own any cellular phones:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_9', array(
       '0' => 'No',
       '6' => 'Yes',
     )) !!}
-<<<<<<< HEAD
   </div>
 </div>
 <div class="form-group row">
   {!! Form::label('question_10', 'PPI10: How many beds and mattresses does your household own', array('class' => 'col-md-5 form-control-label text-right')) !!}
   <div class="col-md-2">
-=======
-    <span class="help-block"></br></span>
-  </div>
-</div>
-<div class="form-group row">
-  {!! Form::label('question_10', 'How many beds and mattresses does your household own:', array('class' => 'col-md-6 form-control-label')) !!}
-  <div class="col-md-4">
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
     {!! Form::select('question_10', array(
       '0' => 'None',
       '2' => 'One or more beds, but no mattresses',
       '4' => 'One mattress (regardless of beds)',
       '7' => 'Two or more mattresses (regardless of beds)',
     )) !!}
-<<<<<<< HEAD
-=======
-    <span class="help-block"></br></span>
->>>>>>> 8f31714ecfb679de06b1a1ab7217e76278718dc9
   </div>
 </div>
 
